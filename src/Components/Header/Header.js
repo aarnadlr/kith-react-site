7d0de94ca468d00b3eb25870b8053ee8468da57a
Aaron Adler <aaronadler@Aarons-MBP.home>
2018-04-13 22:39:33 -0400

    Completed header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,46 +2,84 @@ import React from 'react';
 import './Header.css';
 import styled from 'styled-components';
 import KithLogo from '../KithLogo';
-
-// import fontawesome from '@fortawesome/fontawesome';
-// import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-
-// import faCoffee from '@fortawesome/fontawesome-free-solid/faCoffee';
-// import {faFlag, faSquare } from '@fortawesome/fontawesome-free-solid/';
-
-// fontawesome.library.add(faSquare, faCoffee, faFlag);
-
-// const element = (
-//   <FontAwesomeIcon icon={faCoffee} />
-// )
+import { screen } from '../../css/media-queries';
 
 
 const Container = styled.div`
+  box-sizing: border-box;
+  width: 100%;
   font-family: Space Mono;
-  font-size: .8rem;
-  padding: 100px;
+  text-align: center;
+
+  /* padding: 100px; */
   /* background: transparent; */
 `
 
 const HorizontalNav = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  /* flex-direction:column; */
+  text-align: center;
+  align-items: center;
+  @media ${screen.width700} {
+    flex-wrap: nowrap;
+    
+  }
+  @media ${screen.width800} {
+   padding: 0 80px;
+   /* justify-items: between; */
+  }
 `
+
+
+const NavItem = styled.p`
+  clear: both;
+  font-size: 1rem;
+  text-align: center;
+  display: block;
+  margin: 0;
+  padding: 20px;
+  width: 50%;
+  float:left;
+  border: 0px solid gray;
+  /* visibility: hidden; */
+  @media ${screen.width700} {
+
+    /* width: calc(100% / 8); */
+  }
+  @media ${screen.width800} {
+    width:100%;
+    /* text-align: left; */
+    margin: 50px 0 100px 0;
+    padding:0;
+    font-size: .8rem;
+  }
+
+`
+
+
 
 
 const Header = () => (
   <Container>
 
-    <HorizontalNav className="flex justify-between items-center">
-    <p className='navItem hoverMagenta' >Men</p>
-    <p className='navItem hoverMagenta' >Women</p>
-    <p className='navItem hoverMagenta' >Kids</p>
-    <p className='navItem hoverMagenta' >Treats</p>
+    <KithLogo />
 
-    <KithLogo width={'100px'} />
+    <HorizontalNav className="">
 
-    <p className='navItem hoverMagenta' >Blog</p>
-    <p className='navItem hoverMagenta' >Art</p>
-    <p className='navItem hoverMagenta' >Locations</p>
-    <p className='navItem hoverMagenta' >About</p>
+      
+      <NavItem className='hoverMagenta' >Men</NavItem>
+      <NavItem className='hoverMagenta' >Women</NavItem>
+      <NavItem className='hoverMagenta' >Kids</NavItem>
+      <NavItem className='hoverMagenta' >Treats</NavItem>
+
+
+      <NavItem className='hoverMagenta' >Blog</NavItem>
+      <NavItem className='hoverMagenta' >Art</NavItem>
+      <NavItem className='hoverMagenta' >Locations</NavItem>
+      <NavItem className='hoverMagenta' >About</NavItem>
+
     </HorizontalNav>
 
   </Container>
