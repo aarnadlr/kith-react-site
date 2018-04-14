28c4c1c1bec00add04b3e143fa2ff2f972a55c83
Aaron Adler <aaronadler@Aarons-MacBook-Pro.local>
2018-04-14 17:39:33 -0400

    Configured react-snapshot and rendered.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
+// registerServiceWorker();
